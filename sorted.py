414f1f36f3dab0a0c3211967500afc5718fd47ba
Fangxin Zhu <878237422@qq.com>
2019-07-17 10:07:57 +0800

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -4,11 +4,11 @@
 #练习1: 假设我们用一组tuple表示学生名字和成绩： L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)], 请用sorted()对上述列表分别按名字排序
 
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
-#def by_name(t):
-#    return t[0]
-#
-#L2 = sorted(L, key=by_name)
-#print(L2)	
+def by_name(t):
+    return t[0]
+
+L2 = sorted(L, key=by_name)
+print(L2)	
 
 #练习2: 再按成绩从高到低排序
 def by_score(t):
