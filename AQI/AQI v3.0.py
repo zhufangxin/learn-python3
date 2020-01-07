15e39d8dc91ba0beb5d4b615902ae921c197d6f7
Fangxin Zhu <878237422@qq.com>
2020-01-07 15:18:25 +0800

    Add files via upload
    write to csv file

---
+++ AQI/AQI v3.0.py
@@ -0,0 +1,42 @@
+"""
+功能：读取json文件，并转换成csv文件
+Notes:
+    csv:
+    以行为单位
+    以逗号分割每列数据，即使数据为空也要保留逗号
+    csv.writerow(list)
+
+版本：v2.0
+"""
+
+import json
+import csv
+
+
+def process_json_file(file_path):
+    """
+    解析json文件
+    """
+    f = open(file_path, mode='r', encoding='utf-8')
+    city_list = json.load(f)
+    return city_list
+
+
+def main():
+    file_path = input('请输入json文件名称：')
+    city_list = process_json_file(file_path)
+    city_list.sort(key=lambda city: city['aqi'])
+    lines = []
+    # 列名
+    lines.append(list(city_list[0].keys()))
+    for city in city_list:
+        lines.append(list(city.values()))
+    f = open('aqi.csv', 'w', encoding='utf-8', newline='')  # newline=''末尾不加任何空行
+    writer = csv.writer()
+    for line in lines:
+        writer.writerow(line)
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
