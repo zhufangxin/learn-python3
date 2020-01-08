f3b2446c54bd0b7a8c7859a75b2f733b5c96822b
Fangxin Zhu <878237422@qq.com>
2020-01-08 14:01:25 +0800

    Update AQI v3.0.py

--- AQI/AQI v3.0.py
+++ AQI/AQI v3.0.py
@@ -31,8 +31,8 @@ def main():
     lines.append(list(city_list[0].keys()))
     for city in city_list:
         lines.append(list(city.values()))
-    f = open('aqi.csv', 'w', encoding='utf-8', newline='')  # newline=''末尾不加任何空行
-    writer = csv.writer()
+    f = open('aqi.csv', 'w', encoding='utf-8', newline='')  # 默认每行后加一行空行，newline=''指定末尾不加任何空行
+    writer = csv.writer(f)
     for line in lines:
         writer.writerow(line)
     f.close()
